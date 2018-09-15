cac142a949112512f4602c1c8e58cac9ae65b361
徐瑞婷 <1263183452@qq.com>
2018-09-15 21:08:04 +0800

    H5-插入音频

---
+++ audio/work01audio.js
@@ -0,0 +1,17 @@
+
+function play(){
+	var play= document.getElementById('playaudio');
+	var pause= document.getElementById('pauseaudio');
+	var audio = document.getElementById('audio');
+	audio.play();
+//	play.disabled = true;	
+//	pause.disabled = false;
+	}
+function pause(){
+	var audio = document.getElementByIa('audio');
+	var playb= document.getElementById('playaudio');
+	var pause = document.getElementById('pauseaudio');
+//	pause.disabled = true;
+//	play.disabled = false;
+	audio.pause();
+}
